e124c67d9f4429fa52d3986d2b0888318b8d8e55
CJ Harries <cj@wizardsoftheweb.pro>
2017-10-07 14:56:06 -0500

    Create spec for DecoratorImplementor

---
+++ test/DecoratorImplementor.spec.ts
@@ -0,0 +1,30 @@
+/* tslint:disable:no-console */
+// Things like ...be.true; or ...be.rejected; dont play nice with TSLint
+/* tslint:disable:no-unused-expression */
+import * as chai from "chai";
+// Needed for describe, it, etc.
+import { } from "mocha";
+import * as proxyquire from "proxyquire";
+import * as sinon from "sinon";
+import * as sinonChai from "sinon-chai";
+
+import { InheritsCliDecoratorOptions } from "../src/lib/InheritsCliDecoratorOptions";
+
+const should = chai.should();
+chai.use(sinonChai);
+
+const DecoratorImplementor = proxyquire("../src/lib/DecoratorImplementor", {}).DecoratorImplementor;
+
+describe("DecoratorImplementor", (): void => {
+    let decorateStub: sinon.SinonStub;
+    let resolver: any;
+
+    beforeEach((): void => {
+        decorateStub = sinon.stub(DecoratorImplementor.prototype as any, "decorate");
+        resolver = new DecoratorImplementor({} as any);
+    });
+
+    afterEach((): void => {
+        decorateStub.restore();
+    });
+});
