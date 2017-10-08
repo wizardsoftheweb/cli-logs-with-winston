1acca8f8d07e14c099fd9b05f51cf553c7880969
CJ Harries <cj@wizardsoftheweb.pro>
2017-10-07 23:48:22 -0500

    Document class

--- src/lib/WinstonImportValidator.ts
+++ src/lib/WinstonImportValidator.ts
@@ -1,14 +1,36 @@
 import { InheritsCliDecoratorOptions } from "./InheritsCliDecoratorOptions";
 import { ICliDecoratorOptions } from "./interfaces";
 
+/**
+ * Ensures winston is imported and usable.
+ * @class WinstonImportValidator
+ * @extends InheritsCliDecoratorOptions
+ */
 export class WinstonImportValidator extends InheritsCliDecoratorOptions {
+    /** @type {string} The contents of the class file to validate */
     public contents: string;
 
+    /**
+     * Checks the given file for `winston` and imports if not found.
+     *
+     * @param {string}               contents
+     * Path to the class to validate
+     * @param {ICliDecoratorOptions} options
+     * The options to use
+     */
     public constructor(contents: string, options: ICliDecoratorOptions) {
         super(options);
         this.contents = this.checkWinstonImport(contents);
     }
 
+    /**
+     * Prepends a `winston` import at the top of the file.
+     *
+     * @param  {string} contents
+     * File to prepend
+     * @return {string}
+     * Contents with prepended `winston` import
+     */
     private createWinstonImport(contents: string): string {
         this.options.logger.verbose("Adding winston import");
         return `\
@@ -17,6 +39,15 @@ ${this.options.eol}\
 ${contents}`;
     }
 
+    /**
+     * Checks the found `winston` import for `LoggerInstance`.
+     *
+     * @param  {string} imports
+     * A(n unexploded array inside the) string containing all imported `winston`
+     * members
+     * @return {string}
+     * A sorted list of imports now containing `LoggerInstance`
+     */
     private findOrInsertLoggerInstanceImport(imports: string): string {
         this.options.logger.silly("Parsing winston imports");
         let foundLoggerInstance = false;
@@ -40,6 +71,15 @@ ${this.options.eol}\
 ${this.options.indent}${importedClasses.join("," + this.options.eol + this.options.indent)},${this.options.eol}`;
     }
 
+    /**
+     * Searches contents for a `winston` and `LoggerInstance` import, creating
+     * what it cannot find.
+     *
+     * @param  {string} contents
+     * Contents to check
+     * @return {string}
+     * Contents with the proper imports
+     */
     private checkWinstonImport(contents: string): string {
         this.options.logger.silly("Checking for winston import");
         if (contents.match(/^\s*import[\s\S]*?['"]winston['"];\s*$/gyi)) {
